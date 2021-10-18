28ce7feaf40f37d09c5825140a901b7d47b3a007
Syed Ali <Syed.Waqqas.Ali@gmail.com>
2021-10-18 19:14:10 -0400

    Test rendering release and deadline sprites

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #pragma comment(lib,"sfml-main.lib")
 
 #include <SFML/Graphics.hpp>
+#include "JobReleaseSprite.hpp"
+#include "JobDeadlineSprite.hpp"
 
 using namespace sf;
 
@@ -11,6 +13,11 @@ int main() {
 	RenderWindow window(VideoMode(1280, 720), "COMP 737 Task Scheduling Simulator");
 	const Color clearColor = { 255, 255, 204 };
 	window.setFramerateLimit(100);
+
+	Sprite* jobRelease = JobReleaseSprite::Instance()->createSprite();
+	jobRelease->setPosition(Vector2f(100.f, 100.f));
+	Sprite* jobDeadline = JobDeadlineSprite::Instance()->createSprite();
+	jobDeadline->setPosition(Vector2f(200.f, 100.f));
 
 	while (window.isOpen()) {
 		Event e;
@@ -21,6 +28,8 @@ int main() {
 
 		window.clear(clearColor);
 		// Draw here
+		window.draw(*jobRelease);
+		window.draw(*jobDeadline);
 
 		window.display();
 	}
