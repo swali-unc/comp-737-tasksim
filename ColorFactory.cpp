540f82ef8b2d0974c0b62723e2dc00ddddf821d7
swali-unc <swali@live.unc.edu>
2021-11-06 10:55:20 -0400

    documentation

--- ColorFactory.cpp
+++ ColorFactory.cpp
@@ -3,9 +3,9 @@
 using namespace sf;
 
 static Color colorList[] = {
-	Color(204,236,255),
-	Color(255,204,204),
-	Color(204,255,204),
+	Color(204,236,255), // light blue
+	Color(255,204,204), // light red
+	Color(204,255,204), // light green
 };
 
 Color ColorFactory::getNextColor() {
