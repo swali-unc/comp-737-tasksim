cc2abc510994390ffb3da29c53c38e304665ddac
swali-unc <swali@live.unc.edu>
2021-11-10 17:22:21 -0500

    add more colors

--- ColorFactory.cpp
+++ ColorFactory.cpp
@@ -6,6 +6,11 @@ static Color colorList[] = {
 	Color(204,236,255), // light blue
 	Color(255,204,204), // light red
 	Color(204,255,204), // light green
+	Color(255,204,150), // orange
+	Color(200,191,230), // light purple
+	Color(218,184,165), // light brown
+	Color(204,204,204), // light gray
+	Color(112,146,190), // darker blue
 };
 
 Color ColorFactory::getNextColor() {
